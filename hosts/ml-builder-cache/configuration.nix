140a9d8f776c2b58fdcfa1c95fbe123e531f7455
moli-pp <molishanguang@outlook.com>
2026-07-05 23:12:24 +0800

    feat: update ml-builder-cache configuration and hardware settings

--- hosts/ml-builder-cache/configuration.nix
+++ hosts/ml-builder-cache/configuration.nix
@@ -1,4 +1,10 @@
-{ LT, ... }:
+{
+  LT,
+  config,
+  inputs,
+  pkgs,
+  ...
+}:
 {
   imports = [
     ../../nixos/minimal.nix
@@ -6,11 +12,86 @@
     ./hardware-configuration.nix
   ];
 
-  systemd.network.networks.eth0 = {
+  systemd.network.networks.ens18 = {
     address = [ "${LT.this.interconnect.IPv4}/24" ];
-    gateway = [ "192.168.3.1" ];
-    matchConfig.Name = "eth0";
+    gateway = [ "192.168.2.2" ];
+    matchConfig.Name = "ens18";
     networkConfig.IPv6AcceptRA = "yes";
     ipv6AcceptRAConfig.DHCPv6Client = "no";
   };
+
+  sops.secrets.attic-credentials = {
+    sopsFile = inputs.secrets + "/common/attic.yaml";
+    owner = "atticd";
+    group = "atticd";
+  };
+
+  users.users.atticd = {
+    isSystemUser = true;
+    group = "atticd";
+  };
+  users.groups.atticd = { };
+
+  services.postgresql = {
+    enable = true;
+    ensureDatabases = [ "atticd" ];
+    ensureUsers = [
+      {
+        name = "atticd";
+        ensureDBOwnership = true;
+      }
+    ];
+  };
+
+  services.atticd = {
+    enable = true;
+    package = pkgs.nur-xddxdd.lantianCustomized.attic-telnyx-compatible;
+    environmentFile = config.sops.secrets.attic-credentials.path;
+    mode = "monolithic";
+
+    settings = {
+      listen = "0.0.0.0:${LT.portStr.Attic}";
+      api-endpoint = "http://${LT.this.interconnect.IPv4}:${LT.portStr.Attic}/";
+      substituter-endpoint = "http://${LT.this.interconnect.IPv4}:${LT.portStr.Attic}/";
+
+      database = {
+        url = "postgres://atticd?host=/run/postgresql&user=atticd";
+        heartbeat = true;
+      };
+
+      require-proof-of-possession = false;
+
+      storage = {
+        type = "s3";
+        region = "us-east-1";
+        bucket = "nix-cache";
+        endpoint = "https://rustfs.zhyi.cc:4000";
+      };
+
+      # Keep the author's S3 direct-download style: do not split NARs into chunks.
+      chunking = {
+        nar-size-threshold = 0;
+        min-size = 16384;
+        avg-size = 65536;
+        max-size = 262144;
+      };
+
+      compression = {
+        type = "zstd";
+        level = 9;
+      };
+
+      garbage-collection = {
+        interval = "12 hours";
+        default-retention-period = "3 month";
+      };
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [ LT.port.Attic ];
+
+  environment.systemPackages = with pkgs; [
+    attic-client
+    attic-server
+  ];
 }
