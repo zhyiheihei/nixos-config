a742b7101d2b9f46f048e2a6ab259af22bfbda40
moli-pp <molishanguang@outlook.com>
2026-07-10 10:16:03 +0800

    Use zhyi Hydra URL in watchdog

--- nixos/optional-apps/hydra/watchdog.nix
+++ nixos/optional-apps/hydra/watchdog.nix
@@ -1,13 +1,11 @@
 {
   lib,
   LT,
-  config,
   pkgs,
   ...
 }:
 let
   py = pkgs.python3.withPackages (ps: [ ps.requests ]);
-  hydraBaseUrl = "http://${config.services.hydra.listenHost}:${LT.portStr.Hydra}";
 in
 {
   systemd.services.hydra-watchdog = {
@@ -15,11 +13,6 @@ in
     wantedBy = [ "multi-user.target" ];
 
     path = [ pkgs.systemd ];
-
-    environment = {
-      HYDRA_QUEUE_URL = "${hydraBaseUrl}/queue";
-      HYDRA_STATUS_URL = "${hydraBaseUrl}/status";
-    };
 
     serviceConfig = LT.serviceHarden // {
       Type = "simple";
