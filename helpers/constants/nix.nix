795597c829ceb00b4ddf130e1fb6850e5cb256a7
moli-pp <molishanguang@outlook.com>
2026-07-10 14:31:40 +0800

    移除不必要的公钥和依赖项，简化配置

--- helpers/constants/nix.nix
+++ helpers/constants/nix.nix
@@ -13,8 +13,6 @@ _: rec {
     "https://comfyui.cachix.org"
   ];
   trusted-public-keys = [
-    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-    "cache.nixos.org-1:6NCHdD59X431o0gWJ0qOeuKX2w8VxlNjY36Heq3v4F4="
     attic.publicKey
     "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
     "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
