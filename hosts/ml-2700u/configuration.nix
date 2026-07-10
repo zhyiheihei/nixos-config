b58fa493ab5c686f4e077d72595b97d17c2f319f
moli-pp <molishanguang@outlook.com>
2026-07-11 00:44:37 +0800

    ml-2700u: adopt persistent nix disk layout

--- hosts/ml-2700u/configuration.nix
+++ hosts/ml-2700u/configuration.nix
@@ -16,13 +16,6 @@
     device = "nodev";
   };
 
-  fileSystems."/" = lib.mkForce {
-    device = "/dev/disk/by-uuid/44ccda95-c6bc-4b56-b50e-da3deb16e6c8";
-    fsType = "ext4";
-  };
-
-  sops.age.sshKeyPaths = lib.mkForce [ "/etc/ssh/ssh_host_ed25519_key" ];
-
   services.openssh.settings = {
     PasswordAuthentication = lib.mkOverride 40 true;
     PermitRootLogin = lib.mkOverride 40 "yes";
