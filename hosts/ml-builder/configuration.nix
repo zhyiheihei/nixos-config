11656675634e71cfc041fb47662ce89aee73c60c
moli-pp <molishanguang@outlook.com>
2026-07-12 02:08:12 +0800

    network: proxy Hydra and Nix through OpenClash

--- hosts/ml-builder/configuration.nix
+++ hosts/ml-builder/configuration.nix
@@ -1,4 +1,14 @@
 { pkgs, ... }:
+let
+  proxyEnvironment = {
+    HTTP_PROXY = "http://openclash.zhyi.cc:7892";
+    HTTPS_PROXY = "http://openclash.zhyi.cc:7892";
+    NO_PROXY = "localhost,127.0.0.1,::1,.zhyi.cc,192.168.0.0/16";
+    http_proxy = "http://openclash.zhyi.cc:7892";
+    https_proxy = "http://openclash.zhyi.cc:7892";
+    no_proxy = "localhost,127.0.0.1,::1,.zhyi.cc,192.168.0.0/16";
+  };
+in
 {
   imports = [
     ../../nixos/minimal.nix
@@ -17,6 +27,9 @@
   };
 
   networking.hosts."192.168.2.116" = [ "openclash.zhyi.cc" ];
+
+  environment.variables = proxyEnvironment;
+  systemd.services.nix-daemon.environment = proxyEnvironment;
 
   services.openssh.settings.MaxStartups = "64:30:128";
 
