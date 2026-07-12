19589d9f0455c897f3e7ac76f9e2cfcc446877a6
moli-pp <molishanguang@outlook.com>
2026-07-12 15:55:46 +0800

    nginx: migrate generated vhosts to zhyi domains

--- nixos/common-apps/nginx/testssl.nix
+++ nixos/common-apps/nginx/testssl.nix
@@ -6,10 +6,10 @@ let
       name = lib.head pair;
       prefix = lib.elemAt pair 1;
     in
-    lib.nameValuePair "${name}.lantian.pub" {
-      root = "/nix/sync-servers/www/${name}.lantian.pub";
+    lib.nameValuePair "${name}.zhyi.xin" {
+      root = "/nix/sync-servers/www/${name}.zhyi.xin";
       locations."/".index = "testssl.htm";
-      sslCertificate = "${prefix}-${name}.lantian.pub";
+      sslCertificate = "lets-encrypt-zhyi.xin";
       enableCommonLocationOptions = false;
     };
 in
