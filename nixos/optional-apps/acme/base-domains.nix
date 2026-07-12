423db9261fb5a1ae1d254a7d883aaa0091a3ba0e
moli-pp <molishanguang@outlook.com>
2026-07-12 14:58:33 +0800

    acme: use zhyi domain layout

--- nixos/optional-apps/acme/base-domains.nix
+++ nixos/optional-apps/acme/base-domains.nix
@@ -11,22 +11,18 @@ let
     mkZeroSSLWildcardCert
     ;
 
-  hostSubdomains = lib.mapAttrsToList (n: v: "${n}.xuyh0120.win") LT.hosts;
+  baseDomains = [
+    "zhyi.xin"
+    "zhyi.cc"
+    "moliy.site"
+  ];
+
+  hostSubdomains = lib.mapAttrsToList (n: _: "${n}.zhyi.cc") LT.hosts;
 in
 {
   security.acme.certs = lib.mergeAttrsList (
-    [
-      (mkLetsEncryptWildcardCert "lantian.pub")
-      (mkLetsEncryptWildcardCert "xuyh0120.win")
-      (mkLetsEncryptWildcardCert "56631131.xyz")
-      (mkLetsEncryptWildcardCert "ltn.pw")
-      (mkLetsEncryptWildcardCert "xn--gmqs02au1c935d.pub")
-      (mkZeroSSLWildcardCert "lantian.pub")
-      (mkZeroSSLWildcardCert "xuyh0120.win")
-      (mkZeroSSLWildcardCert "56631131.xyz")
-      (mkZeroSSLWildcardCert "ltn.pw")
-      (mkZeroSSLWildcardCert "xn--gmqs02au1c935d.pub")
-    ]
+    (builtins.map mkLetsEncryptWildcardCert baseDomains)
+    ++ (builtins.map mkZeroSSLWildcardCert baseDomains)
     ++ (builtins.map mkLetsEncryptWildcardCert hostSubdomains)
     ++ (builtins.map mkZeroSSLWildcardCert hostSubdomains)
   );
