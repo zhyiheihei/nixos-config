9229028bbf28288074b22763213ea0ab33cea5c2
moli-pp <molishanguang@outlook.com>
2026-07-13 00:17:21 +0800

    networking: route internal Attic directly

--- hosts/ml-builder-cache/configuration.nix
+++ hosts/ml-builder-cache/configuration.nix
@@ -7,10 +7,10 @@ let
   proxyEnvironment = {
     HTTP_PROXY = "http://openclash.zhyi.cc:7892";
     HTTPS_PROXY = "http://openclash.zhyi.cc:7892";
-    NO_PROXY = "localhost,127.0.0.1,::1,.zhyi.cc,192.168.0.0/16";
+    NO_PROXY = "localhost,127.0.0.1,::1,.zhyi.cc,.zhyi.xin,192.168.0.0/16";
     http_proxy = "http://openclash.zhyi.cc:7892";
     https_proxy = "http://openclash.zhyi.cc:7892";
-    no_proxy = "localhost,127.0.0.1,::1,.zhyi.cc,192.168.0.0/16";
+    no_proxy = "localhost,127.0.0.1,::1,.zhyi.cc,.zhyi.xin,192.168.0.0/16";
   };
 in
 {
@@ -32,6 +32,7 @@ in
 
   networking.hosts = {
     "192.168.2.116" = [ "openclash.zhyi.cc" ];
+    "192.168.2.188" = [ "attic.zhyi.xin" ];
     "192.168.2.192" = [ "ml-builder.zhyi.cc" ];
   };
 
