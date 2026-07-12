9cdab3b403cf2e9d1879be2911e6b24e02c11adb
moli-pp <molishanguang@outlook.com>
2026-07-12 19:22:21 +0800

    configuration: simplify NO_PROXY settings in proxyEnvironment

--- hosts/ml-builder-cache/configuration.nix
+++ hosts/ml-builder-cache/configuration.nix
@@ -7,10 +7,10 @@ let
   proxyEnvironment = {
     HTTP_PROXY = "http://openclash.zhyi.cc:7892";
     HTTPS_PROXY = "http://openclash.zhyi.cc:7892";
-    NO_PROXY = "localhost,127.0.0.1,::1,.zhyi.cc,192.168.0.0/16,cache.nixos.org,cache.nixos-cuda.org,cache.garnix.io,.cachix.org,mirrors.tuna.tsinghua.edu.cn";
+    NO_PROXY = "localhost,127.0.0.1,::1,.zhyi.cc,192.168.0.0/16";
     http_proxy = "http://openclash.zhyi.cc:7892";
     https_proxy = "http://openclash.zhyi.cc:7892";
-    no_proxy = "localhost,127.0.0.1,::1,.zhyi.cc,192.168.0.0/16,cache.nixos.org,cache.nixos-cuda.org,cache.garnix.io,.cachix.org,mirrors.tuna.tsinghua.edu.cn";
+    no_proxy = "localhost,127.0.0.1,::1,.zhyi.cc,192.168.0.0/16";
   };
 in
 {
