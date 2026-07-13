d1a2a8ac0b70a2b203817301c8da8e7f69cddd9a
moli-pp <molishanguang@outlook.com>
2026-07-13 13:53:57 +0800

    Gate account services on credentials

--- nixos/optional-apps/ncps.nix
+++ nixos/optional-apps/ncps.nix
@@ -7,12 +7,12 @@
   services.ncps = {
     enable = true;
     server.addr = "${LT.this.interconnect.IPv4}:${LT.portStr.Ncps}";
-    upstream = {
-      caches = LT.constants.nix.substituters ++ [ "https://cache.nixos.org" ];
-      publicKeys = LT.constants.nix.trusted-public-keys;
-    };
     cache = {
       inherit (config.networking) hostName;
+      upstream = {
+        urls = LT.constants.nix.substituters ++ [ "https://cache.nixos.org" ];
+        publicKeys = LT.constants.nix.trusted-public-keys;
+      };
       lru.schedule = "53 4 * * *";
       maxSize = "100G";
       signNarinfo = false;
