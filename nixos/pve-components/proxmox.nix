6f0146faa0d7ae4167b519e63036f019d020db6d
moli-pp <molishanguang@outlook.com>
2026-07-16 14:50:50 +0800

    fix: enable PVE LXC container runtime

--- nixos/pve-components/proxmox.nix
+++ nixos/pve-components/proxmox.nix
@@ -7,6 +7,26 @@
   # PVE starts restore helpers from pvedaemon's isolated PATH.
   systemd.services.pvedaemon.path = [ pkgs.e2fsprogs ];
 
+  # proxmox-nixos does not import its optional container module by default.
+  # PVE invokes this unit when starting an LXC container through pct.
+  systemd.services."pve-container@" = {
+    description = "PVE LXC Container: %i";
+    unitConfig = {
+      DefaultDependencies = false;
+      Documentation = "man:lxc-start man:lxc man:pct";
+    };
+    serviceConfig = {
+      Delegate = true;
+      ExecStart = "${pkgs.lxc}/bin/lxc-start -F -n %i";
+      ExecStop = "${pkgs.pve-container}/share/lxc/pve-container-stop-wrapper %i";
+      KillMode = "mixed";
+      StandardError = "file:/run/pve/ct-%i.stderr";
+      StandardOutput = "null";
+      TimeoutStopSec = 120;
+      Type = "simple";
+    };
+  };
+
   systemd.services.pvescheduler.serviceConfig = {
     Restart = "always";
     RestartSec = 5;
