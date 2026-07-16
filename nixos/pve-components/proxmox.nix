d1e696a4fb74dc2d3ce953880a5fa36df94b029e
moli-pp <molishanguang@outlook.com>
2026-07-16 13:09:38 +0800

    feat: 添加 pkgs 依赖到 pvedaemon 服务配置

--- nixos/pve-components/proxmox.nix
+++ nixos/pve-components/proxmox.nix
@@ -1,8 +1,11 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 {
   imports = [ ../hardware/vfio.nix ];
 
   services.proxmox-ve.enable = true;
+
+  # PVE starts restore helpers from pvedaemon's isolated PATH.
+  systemd.services.pvedaemon.path = [ pkgs.e2fsprogs ];
 
   systemd.services.pvescheduler.serviceConfig = {
     Restart = "always";
