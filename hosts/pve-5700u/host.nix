0977eda64f5ba49cb3a141296904e0a761abcb1b
moli-pp <molishanguang@outlook.com>
2026-07-16 17:11:31 +0800

    feat: 更新网络配置，添加物理上行链路和文件系统设置

--- hosts/pve-5700u/host.nix
+++ hosts/pve-5700u/host.nix
@@ -5,5 +5,6 @@
   city = geo.cities."CN Ningbo";
   cpuThreads = 16;
   hostname = "pve-5700u.zhyi.cc";
+  ssh.ed25519 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICo2gngU3agJnmKjwtp6qLF5YZH1EhmON8tKmdDyOGBd";
   manualDeploy = true;
 }
