d6a753824d29c75b1ef8c282aab0d47dfb00a580
moli-pp <molishanguang@outlook.com>
2026-07-17 06:08:01 +0800

    network: avoid fragmentation on nested mesh links

--- nixos/server-apps/wg-mesh.nix
+++ nixos/server-apps/wg-mesh.nix
@@ -7,21 +7,22 @@
 }:
 let
   wg-pubkey = import (inputs.secrets + "/wg-pubkey.nix");
-  wgEndpointFor =
+  useZeroTierFor =
     name: host:
     let
       sharedInterconnect =
         LT.interconnectIPv4For name != null || LT.interconnectIPv6For name != null;
-      useZeroTier =
-        !sharedInterconnect
-        && LT.this.zerotier != null
-        && host.zerotier != null
-        && (
-          (LT.this.public.IPv4 == null && LT.this.public.IPv6 == null)
-          || (host.public.IPv4 == null && host.public.IPv6 == null)
-        );
     in
-    if useZeroTier then
+    !sharedInterconnect
+    && LT.this.zerotier != null
+    && host.zerotier != null
+    && (
+      (LT.this.public.IPv4 == null && LT.this.public.IPv6 == null)
+      || (host.public.IPv4 == null && host.public.IPv6 == null)
+    );
+  wgEndpointFor =
+    name: host:
+    if useZeroTierFor name host then
       host.ltnet.IPv4
     else if !(LT.this.hasTag "ipv6-only") && LT.publicIPv4For name != null then
       LT.publicIPv4For name
@@ -87,7 +88,7 @@ in
       ++ lib.optionals (LT.this.ltnet.IPv4 != null) [ (LT.this.ltnet.IPv4 + "/32") ]
       ++ lib.optionals (LT.this.ltnet.IPv6 != null) [ (LT.this.ltnet.IPv6 + "/128") ];
 
-      linkConfig.MTUBytes = 1400;
+      linkConfig.MTUBytes = if useZeroTierFor n v then 1280 else 1400;
       networkConfig = {
         LinkLocalAddressing = "no";
       };
