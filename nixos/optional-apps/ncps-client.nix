9a97d358250c0caf9b45de95bf1b798a7b59926d
moli-pp <molishanguang@outlook.com>
2026-07-17 04:54:22 +0800

    ncps: keep private Attic as the primary cache

--- nixos/optional-apps/ncps-client.nix
+++ nixos/optional-apps/ncps-client.nix
@@ -2,6 +2,7 @@
 {
   services.nix-cache-proxy.enable = lib.mkForce false;
   nix.settings.substituters = lib.mkForce [
+    LT.nix.attic.url
     "http://${LT.hosts."ml-home-vm".interconnect.IPv4}:${LT.portStr.Ncps}"
   ];
 }
