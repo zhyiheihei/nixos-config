c1f6e84d4d522f3281aea3ee0f45b29e77353967
moli-pp <molishanguang@outlook.com>
2026-07-18 21:05:04 +0800

    apps: protect migrated panels with oauth

--- nixos/optional-apps/filecodebox.nix
+++ nixos/optional-apps/filecodebox.nix
@@ -28,7 +28,10 @@
 
     lantian.nginxVhosts = {
       "filebox.zhyi.xin" = {
-        locations."/".proxyPass = "http://127.0.0.1:${LT.portStr.FileCodeBox}";
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:${LT.portStr.FileCodeBox}";
+          enableOAuth = true;
+        };
         sslCertificate = "lets-encrypt-zhyi.xin";
         noIndex.enable = true;
       };
