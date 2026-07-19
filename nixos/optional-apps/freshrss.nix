98b507bdc0385f5741eda9f937b38ce8615b0c1e
moli-pp <molishanguang@outlook.com>
2026-07-19 18:30:16 +0800

    freshrss: add native Dex login

--- nixos/optional-apps/freshrss.nix
+++ nixos/optional-apps/freshrss.nix
@@ -1,4 +1,10 @@
-{ LT, config, lib, ... }:
+{
+  LT,
+  config,
+  inputs,
+  lib,
+  ...
+}:
 {
   options.lantian.freshrss.storage = lib.mkOption {
     type = lib.types.str;
@@ -7,16 +13,40 @@
   };
 
   config = {
+    sops.secrets = {
+      freshrss-oidc-client-secret = {
+        sopsFile = inputs.secrets + "/common/dex.yaml";
+        key = "dex-freshrss-secret";
+      };
+      freshrss-oidc-client-crypto-key = {
+        sopsFile = inputs.secrets + "/common/personal-apps.yaml";
+        key = "FRESHRSS_OIDC_CLIENT_CRYPTO_KEY";
+      };
+    };
+
+    sops.templates.freshrss-env.content = ''
+      OIDC_CLIENT_SECRET=${config.sops.placeholder.freshrss-oidc-client-secret}
+      OIDC_CLIENT_CRYPTO_KEY=${config.sops.placeholder.freshrss-oidc-client-crypto-key}
+    '';
+
     virtualisation.oci-containers.containers.freshrss = {
-      image = "docker.io/linuxserver/freshrss:1.29.1";
+      image = "docker.io/freshrss/freshrss:1.29.1";
       labels."io.containers.autoupdate" = "registry";
       ports = [ "127.0.0.1:${LT.portStr.FreshRSS}:80" ];
-      volumes = [ "${config.lantian.freshrss.storage}:/config" ];
+      volumes = [
+        "${config.lantian.freshrss.storage}/www/freshrss/data:/var/www/FreshRSS/data"
+        "${config.lantian.freshrss.storage}/www/freshrss/extensions:/var/www/FreshRSS/extensions"
+      ];
       environment = {
-        PUID = "1000";
-        PGID = "1000";
         TZ = config.time.timeZone;
+        OIDC_ENABLED = "1";
+        OIDC_PROVIDER_METADATA_URL = "https://login.zhyi.xin/.well-known/openid-configuration";
+        OIDC_CLIENT_ID = "freshrss";
+        OIDC_REMOTE_USER_CLAIM = "preferred_username";
+        OIDC_SCOPES = "openid profile email groups";
+        OIDC_X_FORWARDED_HEADERS = "X-Forwarded-Host X-Forwarded-Port X-Forwarded-Proto";
       };
+      environmentFiles = [ config.sops.templates.freshrss-env.path ];
     };
 
     systemd.tmpfiles.settings.freshrss."${config.lantian.freshrss.storage}"."d" = {
