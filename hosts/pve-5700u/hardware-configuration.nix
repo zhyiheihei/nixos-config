c170e6ddf3123cd0b5c43bf57677e4f77fee2c44
moli-pp <molishanguang@outlook.com>
2026-07-19 12:32:35 +0800

    pve: persist host swapfile

--- hosts/pve-5700u/hardware-configuration.nix
+++ hosts/pve-5700u/hardware-configuration.nix
@@ -57,7 +57,7 @@
     "/nix/persistent/var/lib/vz/virtiofs"
   ];
 
-  swapDevices = [ ];
+  swapDevices = [ { device = "/nix/swap/pve.swap"; } ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
