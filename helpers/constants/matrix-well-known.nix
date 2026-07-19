36b319216a10f56d29a7edfc98b57e9f1567ad44
moli-pp <molishanguang@outlook.com>
2026-07-19 18:01:51 +0800

    添加矩阵服务器名称配置为 zhyi.xin

--- helpers/constants/matrix-well-known.nix
+++ helpers/constants/matrix-well-known.nix
@@ -4,6 +4,7 @@
   client = builtins.toJSON {
     "m.server"."base_url" = "https://matrix.zhyi.xin";
     "m.homeserver"."base_url" = "https://matrix.zhyi.xin";
+    "m.homeserver"."server_name" = "zhyi.xin";
     "m.identity_server"."base_url" = "https://vector.im";
     "org.matrix.msc3575.proxy"."url" = "https://matrix.zhyi.xin";
   };
