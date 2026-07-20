6bf741384eccaea3dacdcef219a73ba96f2bc5ee
moli-pp <molishanguang@outlook.com>
2026-07-19 21:37:23 -0700

    plausible: restore declarative geonames source

--- nixos/optional-apps/plausible.nix
+++ nixos/optional-apps/plausible.nix
@@ -8,6 +8,10 @@
 }:
 let
   netns = config.lantian.netns.plausible;
+  plausiblePackage = inputs.nixpkgs-stable.legacyPackages.${pkgs.stdenv.hostPlatform.system}.plausible;
+  geonames = pkgs.runCommand "plausible-geonames.csv" { } ''
+    cp "$(find ${plausiblePackage}/lib -path '*/priv/geonames.lite.csv' -print -quit)" "$out"
+  '';
 in
 {
   imports = [
@@ -30,7 +34,7 @@ in
   services.plausible = {
     enable = true;
     # Latest version on nixos-unstable times out
-    package = inputs.nixpkgs-stable.legacyPackages.${pkgs.stdenv.hostPlatform.system}.plausible;
+    package = plausiblePackage;
 
     mail = {
       email = config.programs.msmtp.accounts.default.from;
@@ -83,7 +87,7 @@ in
         ERL_EPMD_ADDRESS = "127.0.0.1";
 
         GEOLITE2_COUNTRY_DB = "/etc/geoip/GeoLite2-Country.mmdb";
-        GEONAMES_SOURCE_FILE = "/var/lib/plausible/geonames.csv";
+        GEONAMES_SOURCE_FILE = geonames;
         IP_GEOLOCATION_DB = "/etc/geoip/GeoLite2-City.mmdb";
 
         STORAGE_DIR = lib.mkForce "/run/plausible/elixir_tzdata";
