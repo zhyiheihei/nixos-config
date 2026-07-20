307c88824c0693a8afc2e9a6855ffc89ead18ce4
moli-pp <molishanguang@outlook.com>
2026-07-20 09:20:39 +0800

    prometheus: monitor self-hosted backup storage

--- nixos/optional-apps/prometheus/storagebox.nix
+++ nixos/optional-apps/prometheus/storagebox.nix
@@ -1,40 +1,26 @@
+{ pkgs, ... }:
 {
-  LT,
-  config,
-  inputs,
-  lib,
-  ...
-}:
-{
-  sops.secrets.hetzner-storagebox-metrics-token = {
-    sopsFile = inputs.secrets + "/hetzner-storagebox-metrics.yaml";
-    owner = config.services.prometheus.exporters.storagebox.user;
-    inherit (config.services.prometheus.exporters.storagebox) group;
-  };
-
-  services.prometheus.exporters.storagebox = {
-    enable = true;
-    port = LT.port.Prometheus.StorageBoxExporter;
-    listenAddress = "127.0.0.1";
-    tokenFile = config.sops.secrets.hetzner-storagebox-metrics-token.path;
-  };
-  systemd.services.prometheus-storagebox-exporter.serviceConfig = {
-    DynamicUser = lib.mkForce false;
-  };
-  users.users.storagebox-exporter = {
-    group = "storagebox-exporter";
-    isSystemUser = true;
-  };
-  users.groups.storagebox-exporter = { };
-
-  services.prometheus.scrapeConfigs = [
-    {
-      job_name = "storagebox_exporter";
-      static_configs = [
-        {
-          targets = [ "127.0.0.1:${builtins.toString config.services.prometheus.exporters.storagebox.port}" ];
-        }
-      ];
-    }
+  services.prometheus.ruleFiles = [
+    (pkgs.writeText "prometheus-storagebox.rules" (
+      builtins.toJSON {
+        groups = [
+          {
+            name = "storagebox";
+            rules = [
+              {
+                record = "storagebox_disk_quota";
+                expr = ''node_filesystem_size_bytes{instance="ml-home-vm",mountpoint="/mnt/storage"}'';
+                labels.name = "ml-home-vm";
+              }
+              {
+                record = "storagebox_disk_usage";
+                expr = ''node_filesystem_size_bytes{instance="ml-home-vm",mountpoint="/mnt/storage"} - node_filesystem_avail_bytes{instance="ml-home-vm",mountpoint="/mnt/storage"}'';
+                labels.name = "ml-home-vm";
+              }
+            ];
+          }
+        ];
+      }
+    ))
   ];
 }
