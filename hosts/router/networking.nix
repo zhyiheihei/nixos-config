41c1e2a920a5b65e068e013005245dc31155644e
moli-pp <molishanguang@outlook.com>
2026-07-24 02:44:09 +0800

    fix(router): persist QNAP compatibility route

--- hosts/router/networking.nix
+++ hosts/router/networking.nix
@@ -58,8 +58,16 @@
       address = [
         "192.168.0.1/24"
         "192.168.0.4/24"
+        "192.168.2.6/32"
         "240e:390:2568:fa81::1/64"
         "fc00:192:168:0::1/64"
+      ];
+      routes = [
+        {
+          Destination = "192.168.2.93/32";
+          PreferredSource = "192.168.2.6";
+          Scope = "link";
+        }
       ];
       linkConfig.MTUBytes = "9000";
       networkConfig.IPv6SendRA = "yes";
