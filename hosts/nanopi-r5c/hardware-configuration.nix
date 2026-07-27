101909467bc9a9811841dbe54b043cee7fdeae9c
moli-pp <molishanguang@outlook.com>
2026-07-27 09:41:26 +0800

    fix: enlarge R5C firmware partition

--- hosts/nanopi-r5c/hardware-configuration.nix
+++ hosts/nanopi-r5c/hardware-configuration.nix
@@ -87,6 +87,7 @@ in
   sdImage = {
     firmwarePartitionOffset = 16;
     firmwarePartitionName = "FIRMWARE";
+    firmwareSize = 256;
     rootFilesystemCreator = ./make-nix-btrfs-fs.nix;
     rootPartitionUUID = "44444444-4444-4444-8888-888888888888";
     rootVolumeLabel = "NIXOS_NIX";
