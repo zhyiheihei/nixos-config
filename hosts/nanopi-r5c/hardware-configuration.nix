efecfe50e10d302a2b27b29dc5ca8cd9edea51ba
moli-pp <molishanguang@outlook.com>
2026-07-27 15:21:58 +0800

    fix(r5c): initialize persistent machine id

--- hosts/nanopi-r5c/hardware-configuration.nix
+++ hosts/nanopi-r5c/hardware-configuration.nix
@@ -118,6 +118,9 @@ in
     '';
     populateRootCommands = lib.mkForce ''
       mkdir -p files/persistent/etc/ssh files/var/nix/profiles
+      # preservation links /etc/machine-id here from the initrd.  Keep the
+      # target empty in the image so systemd assigns a unique ID on first boot.
+      touch files/persistent/etc/machine-id
       ln -s ${config.system.build.toplevel} files/var/nix/profiles/system
     '';
     postBuildCommands = ''
