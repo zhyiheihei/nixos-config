d897e5d9ec014c329085d098134566930920de14
moli-pp <molishanguang@outlook.com>
2026-07-30 15:20:43 +0800

    123

--- hosts/router/host.nix
+++ hosts/router/host.nix
@@ -4,6 +4,7 @@
   system = "aarch64-linux";
   tags = with tags; [ lan-access ];
   cpuThreads = 4;
+  manualDeploy = true;
   city = geo.cities."CN Ningbo";
   ssh.ed25519 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEPCbPTOyCxfjNZV6ATbPWTfp4Xsl2K8gasAcRRN33q+";
   zerotier = "f1de7dca51";
