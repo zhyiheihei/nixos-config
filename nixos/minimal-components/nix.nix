53ebe1d642000655dbe5050293ac422141f5cb32
Zhyi <molishanguang@outlook.com>
2026-08-01 02:21:58 +0800

    align shared modules with author: angrr GC policies and socket-activated iperf3

--- nixos/minimal-components/nix.nix
+++ nixos/minimal-components/nix.nix
@@ -29,6 +29,40 @@ in
     mode = "0444";
   };
 
+  services.angrr = {
+    enable = true;
+    settings = {
+      temporary-root-policies = {
+        direnv = {
+          path-regex = "/\\.direnv/";
+          period = "14d";
+        };
+        result = {
+          path-regex = "/result[^/]*$";
+          period = "3d";
+        };
+      };
+      profile-policies = {
+        system = {
+          profile-paths = [ "/nix/var/nix/profiles/system" ];
+          keep-since = "14d";
+          keep-latest-n = 5;
+          keep-booted-system = true;
+          keep-current-system = true;
+        };
+        user = {
+          enable = false; # Policies can be individually disabled
+          profile-paths = [
+            "~/.local/state/nix/profiles/profile"
+            "/nix/var/nix/profiles/per-user/root/profile"
+          ];
+          keep-since = "1d";
+          keep-latest-n = 1;
+        };
+      };
+    };
+  };
+
   nix = {
     package = pkgs.nixVersions.latest;
     extraOptions = ''
