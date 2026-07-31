79f9dd989e23c71620910344c351add30bbd9007
moli-pp <molishanguang@outlook.com>
2026-08-01 03:04:07 +0800

    jellyfin: route metadata fetches through router v2ray proxy
    TMDB and other metadata providers are unreachable from the jellyfin
    netns directly; use the router v2ray SOCKS client port as HTTP proxy,
    matching the existing ncps proxy pattern.

--- nixos/optional-apps/jellyfin.nix
+++ nixos/optional-apps/jellyfin.nix
@@ -1,10 +1,14 @@
 {
+  LT,
   pkgs,
   config,
   utils,
   ...
 }:
 let
+  proxy = "http://${LT.hosts.router.interconnect.IPv4}:${LT.portStr.V2Ray.SocksClient}";
+  noProxy = "localhost,127.0.0.1,::1,192.168.0.0/16,.zhyi.cc,.zhyi.xin";
+
   loggingConf = {
     Serilog = {
       Using = [ "Serilog.Sinks.Console" ];
@@ -64,6 +68,12 @@ in
       JELLYFIN_kestrel__socketPath = "/run/jellyfin/socket";
       JELLYFIN_kestrel__socketPermissions = "0777";
       JELLYFIN_PublishedServerUrl = "https://jellyfin.zhyi.xin";
+      HTTP_PROXY = proxy;
+      HTTPS_PROXY = proxy;
+      NO_PROXY = noProxy;
+      http_proxy = proxy;
+      https_proxy = proxy;
+      no_proxy = noProxy;
     };
     serviceConfig = {
       RuntimeDirectory = "jellyfin";
