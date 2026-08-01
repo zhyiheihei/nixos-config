bfd45f24a5947788683b48ba466b6c5f0450486f
moli-pp <molishanguang@outlook.com>
2026-08-02 00:46:39 +0800

    media: complete opi5p service cutover

--- hosts/ml-home-vm/media-center.nix
+++ hosts/ml-home-vm/media-center.nix
@@ -1,12 +1,72 @@
-{ lib, ... }:
+{
+  lib,
+  ...
+}:
+let
+  edgeServices = [
+    "bt"
+    "pt"
+    "seedbox"
+    "sonarr"
+    "radarr"
+    "bazarr"
+    "prowlarr"
+    "jproxy"
+    "peerbanhelper"
+    "bitmagnet"
+    "iyuu"
+  ];
+  mkProxyLocation = service: {
+    proxyPass = "https://${service}.opi5p.zhyi.cc";
+    proxyOverrideHost = "${service}.opi5p.zhyi.cc";
+    proxyWebsockets = true;
+    proxyNoTimeout = true;
+  }
+  // lib.optionalAttrs (builtins.elem service [
+    "bt"
+    "pt"
+    "seedbox"
+  ]) { allowCORS = true; };
+  mkEdgeVhosts = service: [
+    {
+      name = "${service}.ml-home-vm.zhyi.cc";
+      value = {
+        locations."/" = mkProxyLocation service;
+        accessibleBy = "private";
+        sslCertificate =
+          if service == "iyuu" then
+            "lets-encrypt-ml-home-vm.zhyi.cc"
+          else
+            "zerossl-ml-home-vm.zhyi.cc";
+        noIndex.enable = true;
+      };
+    }
+    {
+      name = "${service}.localhost";
+      value = {
+        listenHTTP.enable = true;
+        listenHTTPS.enable = false;
+        locations."/" = mkProxyLocation service;
+        accessibleBy = "localhost";
+        noIndex.enable = true;
+      };
+    }
+  ];
+in
 {
   imports = [
     ../../nixos/client-components/hidpi.nix
     ../../nixos/client-components/xorg.nix
 
     ../../nixos/optional-apps/jellyfin.nix
-    ../../nixos/optional-apps/media-automation.nix
   ];
 
   services.xserver.enable = lib.mkForce false;
+
+  # The media writers now run exclusively on OPI5P. Keep the old private
+  # URLs as stable edge endpoints, but never instantiate a second copy of the
+  # downloaders, automation workers or their databases on this host again.
+  lantian.nginxVhosts = builtins.listToAttrs (
+    builtins.concatLists (map mkEdgeVhosts edgeServices)
+  );
 }
