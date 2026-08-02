4b16a953856478c05c1a231ac9b652c42b571748
moli-pp <molishanguang@outlook.com>
2026-08-02 20:11:32 +0800

    cups: keep i686 Brother drivers off aarch64

--- nixos/client-components/cups.nix
+++ nixos/client-components/cups.nix
@@ -1,26 +1,35 @@
-{ pkgs, ... }:
+{
+  lib,
+  pkgs,
+  ...
+}:
 {
   services.printing = {
     enable = true;
     startWhenNeeded = false;
-    drivers = with pkgs; [
-      brgenml1cupswrapper
-      brgenml1lpr
-      brlaser
-      # cnijfilter2
-      # epson-escpr
-      foomatic-db
-      foomatic-db-engine
-      foomatic-db-nonfree
-      foomatic-db-ppds-withNonfreeDb
-      foomatic-filters
-      gutenprint
-      gutenprintBin
-      hplip
-      hplipWithPlugin
-      samsung-unified-linux-driver
-      splix
-    ];
+    drivers =
+      (with pkgs; [
+        brlaser
+        # cnijfilter2
+        # epson-escpr
+        foomatic-db
+        foomatic-db-engine
+        foomatic-db-nonfree
+        foomatic-db-ppds-withNonfreeDb
+        foomatic-filters
+        gutenprint
+        gutenprintBin
+        hplip
+        hplipWithPlugin
+        samsung-unified-linux-driver
+        splix
+      ])
+      # Nixpkgs implements Brother's proprietary driver through pkgsi686Linux,
+      # which cannot be instantiated on native aarch64 hosts.
+      ++ lib.optionals pkgs.stdenv.hostPlatform.isx86_64 (with pkgs; [
+        brgenml1cupswrapper
+        brgenml1lpr
+      ]);
 
     cups-pdf = {
       enable = true;
