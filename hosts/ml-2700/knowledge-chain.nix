f32f295d33322e548c1e9392a485342a76eff5da
moli-pp <molishanguang@outlook.com>
2026-08-10 09:45:08 +0800

    fix(ml-2700): keep normal file modes in Notes bindfs mount
    perms=700 made every file appear executable, so git recorded 100755 modes
    and Syncthing marker files leaked into commits. Use underlying modes and
    create new files as 0644.

--- hosts/ml-2700/knowledge-chain.nix
+++ hosts/ml-2700/knowledge-chain.nix
@@ -13,11 +13,9 @@
     options = LT.constants.bindfsMountOptions' [
       "force-user=zhyi"
       "force-group=zhyi"
-      "perms=700"
       "create-for-user=zhyi"
       "create-for-group=users"
-      "create-with-perms=755"
-      "chmod-ignore"
+      "create-with-perms=0644"
     ];
   };
 
