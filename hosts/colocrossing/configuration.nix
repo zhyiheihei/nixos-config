04d338362b05aa65ad0345d04599b17f2291c973
moli-pp <molishanguang@outlook.com>
2026-08-11 22:54:32 +0800

    fix(colocrossing): reference sublinkpro-nix module file explicitly

--- hosts/colocrossing/configuration.nix
+++ hosts/colocrossing/configuration.nix
@@ -38,7 +38,7 @@
     ../../nixos/optional-apps/radicale.nix
     ../../nixos/optional-apps/rsshub.nix
     ../../nixos/optional-apps/rsync-server-ci.nix
-    ../../nixos/optional-apps/sublinkpro-nix
+    ../../nixos/optional-apps/sublinkpro-nix.nix
     ../../nixos/optional-apps/syncthing
     ../../nixos/optional-apps/tg-bot-cleaner-bot
     ../../nixos/optional-apps/yggdrasil-alfis.nix
