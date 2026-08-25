ed64d9cca43846062471b7cb66d79b348028f6e3
moli-pp <molishanguang@outlook.com>
2026-08-25 21:55:15 +0800

    fix(overlay): python3.override 用 flat attrs + packageOverrides 格式

--- overlays/51-python-downgrade.nix
+++ overlays/51-python-downgrade.nix
@@ -4,25 +4,36 @@
 # 用 packageOverrides 给所有 xstatic-* 包注入 setuptools 到 nativeBuildInputs。
 _: final: prev:
 {
-  python3 = prev.python3.override (
-    self: super: {
-      pkgs = super.pkgs.overrideScope (
-        _final: ssuper: builtins.mapAttrs
-          (name: pkg: pkg.overridePythonAttrs (old: {
-            nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ ssuper.setuptools ];
-          }))
-          {
-            xstatic = ssuper.xstatic;
-            xstatic-asciinema-player = ssuper.xstatic-asciinema-player;
-            xstatic-bootbox = ssuper.xstatic-bootbox;
-            xstatic-bootstrap = ssuper.xstatic-bootstrap;
-            xstatic-font-awesome = ssuper.xstatic-font-awesome;
-            xstatic-jquery = ssuper.xstatic-jquery;
-            xstatic-jquery-file-upload = ssuper.xstatic-jquery-file-upload;
-            xstatic-jquery-ui = ssuper.xstatic-jquery-ui;
-            xstatic-pygments = ssuper.xstatic-pygments;
-          }
-      );
-    }
-  );
+  python3 = prev.python3.override {
+    self = final.python3;
+    packageOverrides = _self: super: {
+      xstatic = super.xstatic.overridePythonAttrs (old: {
+        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ super.setuptools ];
+      });
+      xstatic-asciinema-player = super.xstatic-asciinema-player.overridePythonAttrs (old: {
+        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ super.setuptools ];
+      });
+      xstatic-bootbox = super.xstatic-bootbox.overridePythonAttrs (old: {
+        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ super.setuptools ];
+      });
+      xstatic-bootstrap = super.xstatic-bootstrap.overridePythonAttrs (old: {
+        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ super.setuptools ];
+      });
+      xstatic-font-awesome = super.xstatic-font-awesome.overridePythonAttrs (old: {
+        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ super.setuptools ];
+      });
+      xstatic-jquery = super.xstatic-jquery.overridePythonAttrs (old: {
+        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ super.setuptools ];
+      });
+      xstatic-jquery-file-upload = super.xstatic-jquery-file-upload.overridePythonAttrs (old: {
+        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ super.setuptools ];
+      });
+      xstatic-jquery-ui = super.xstatic-jquery-ui.overridePythonAttrs (old: {
+        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ super.setuptools ];
+      });
+      xstatic-pygments = super.xstatic-pygments.overridePythonAttrs (old: {
+        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ super.setuptools ];
+      });
+    };
+  };
 }
