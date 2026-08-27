c2451895b010b850684fe87df5b55ef0e19cc4d1
moli-pp <molishanguang@outlook.com>
2026-08-27 08:46:36 +0800

    fix(nix.nix): 移除 sjtu 镜像地址

--- helpers/constants/nix.nix
+++ helpers/constants/nix.nix
@@ -21,7 +21,6 @@
     attic.url
     authorCachix.url
     authorAttic.url
-    "https://mirror.sjtu.edu.cn/nix-channels/store"
     "https://mirrors.ustc.edu.cn/nix-channels/store"
     "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
     "https://cache.nixos-cuda.org"
