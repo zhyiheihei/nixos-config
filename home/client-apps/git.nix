596688c68e753d4bcdd4e16779c122fd629614fc
moli-pp <molishanguang@outlook.com>
2026-09-02 01:15:47 +0800

    fix(git): 签名钥匙切到 DAE24FE1，身份统一 moli-pp
    B50EC319385FCB0D 私钥已不在本机，commit.gpgsign=true 导致提交失败。
    签名钥匙改为本机现存的 DAE24FE1（moli-pp），git 身份从 tunings.nix
    单点定义（原 git.nix 与 tunings.nix 双定义会触发 option 冲突），
    user.name/email 由 Magic Flash/xuyh0120 对齐为 moli-pp/molishanguang。

--- home/client-apps/git.nix
+++ home/client-apps/git.nix
@@ -9,7 +9,7 @@
     package = lib.mkForce pkgs.git;
     settings.core.excludesfile = "~/.gitignore";
     signing = {
-      key = "B50EC319385FCB0D";
+      key = "DAE24FE12237C9A4AEC90F0CBD6260B17D94249B";
       format = "openpgp";
       signByDefault = true;
     };
--- home/common-apps/tunings.nix
+++ home/common-apps/tunings.nix
@@ -24,8 +24,8 @@
         autoSetupRemote = true;
       };
       user = {
-        name = "Magic Flash";
-        email = "xuyh0120@outlook.com";
+        name = "moli-pp";
+        email = "molishanguang@outlook.com";
       };
 
       # https://forums.whonix.org/t/git-users-enable-fsck-by-default-for-better-security/2066
